a992be713725100131312f109aa231dc21c05edb
Budhycc <106530190+Budhycc@users.noreply.github.com>
2025-06-02 17:39:16 +0800

    build update

---
+++ dist/app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Menjalankan aplikasi 1TiketKeretaApi
+#!/bin/bash
+
+# Pindah ke direktori tempat script berada
+cd "$(dirname "$0")"
+
+# Jalankan file JAR
+java -jar 1TiketKeretaApi.jar
